d14953d185a84ce5e99a3e6a6a2719ec8e8436bb
BaronBalt <walmgren@kth.se>
2026-05-08 20:41:11 +0200

    immutable

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,13 @@
+using api;
 using Microsoft.Data.SqlClient;
+
+const string getConnectionsSql = 
+    """
+    SELECT host_name, direction, pid, process_name,
+        source_fqdn, source_ip, source_port,
+        target_fqdn, target_ip, target_port
+    FROM inventory.connections
+    """;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,78 +40,59 @@ app.MapGet(
 
         await using var conn = new SqlConnection(connectionString);
         await conn.OpenAsync();
+    
+        var cmd = new SqlCommand(getConnectionsSql, conn);
 
-        var cmd = new SqlCommand(
-            """
-                SELECT host_name, direction, pid, process_name,
-                       source_fqdn, source_ip, source_port,
-                       target_fqdn, target_ip, target_port
-                FROM inventory.connections
-            """,
-            conn
-        );
-
-        var results = new GraphResponse();
+        var nodes = new List<Node>();
+        var edges = new List<Edge>();
         var seenNodes = new HashSet<string>();
 
         await using var reader = await cmd.ExecuteReaderAsync();
         while (await reader.ReadAsync())
         {
-            string? sourceFqdn = reader["source_fqdn"] as string;
-            string? sourceIp = reader["source_ip"] as string;
-
-            string? targetFqdn = reader["target_fqdn"] as string;
-            string? targetIp = reader["target_ip"] as string;
-
-            if (sourceFqdn is null || sourceIp is null || targetFqdn is null || targetIp is null)
+            if (reader["source_fqdn"] is not string sourceFqdn || reader["source_ip"] is not string sourceIp || 
+                reader["target_fqdn"] is not string targetFqdn || reader["target_ip"] is not string targetIp)
             {
                 continue;
             }
 
 
-            string? processName = reader["process_name"] as string;
+            var processName = reader["process_name"] as string;
 
-            int pid = reader["pid"] == DBNull.Value ? -1 : Convert.ToInt32(reader["pid"]);
+            var pid = reader["pid"] == DBNull.Value ? -1 : Convert.ToInt32(reader["pid"]);
 
-            int sourcePort =
+            var sourcePort =
                 reader["source_port"] == DBNull.Value ? 0 : Convert.ToInt32(reader["source_port"]);
 
-            int targetPort =
+            var targetPort =
                 reader["target_port"] == DBNull.Value ? 0 : Convert.ToInt32(reader["target_port"]);
-
-            string sourceNodeId = sourceFqdn;
-            string targetNodeId = targetFqdn;
-
-            if (seenNodes.Add(sourceNodeId))
+            
+            if (seenNodes.Add(sourceFqdn))
             {
-                results.Nodes.Add(new Node { Fqdn = sourceFqdn, Ip = sourceIp });
+                nodes.Add(new Node(sourceFqdn, sourceIp));
             }
-
-            if (seenNodes.Add(targetNodeId))
+            
+            if (seenNodes.Add(targetFqdn))
             {
-                results.Nodes.Add(new Node { Fqdn = targetFqdn, Ip = targetIp });
+                nodes.Add(new Node(targetFqdn, targetIp));
             }
 
 
-            results.Edges.Add(
-                new Edge
-                {
-                    Id = $"{sourceFqdn}:{sourcePort}->{targetFqdn}:{targetPort}",
-
-                    SourceIp = sourceIp,
-                    SourcePort = sourcePort,
-                    SourceFqdn = sourceFqdn,
-
-                    TargetIp = targetIp,
-                    TargetPort = targetPort,
-                    TargetFqdn = targetFqdn,
-
-                    Pid = pid,
-                    ProcessName = processName,
-                }
+            edges.Add(
+                new Edge(
+                    Id: $"{sourceFqdn}:{sourcePort}->{targetFqdn}:{targetPort}",
+                    SourceIp: sourceIp,
+                    SourcePort: sourcePort,
+                    SourceFqdn: sourceFqdn,
+                    TargetIp: targetIp,
+                    TargetPort: targetPort,
+                    TargetFqdn: targetFqdn,
+                    Pid: pid,
+                    ProcessName: processName
+                )
             );
         }
-        return Results.Ok(results);
+        return Results.Ok(new GraphResponse(nodes, edges));
     }
 );
 
